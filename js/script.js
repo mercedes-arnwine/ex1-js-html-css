54c14d90ea9d4e55e3a1596ca464c247a80e7f62
mercedes-arnwine <80288739+mercedes-arnwine@users.noreply.github.com>
2023-01-15 20:47:14 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var footer = document.querySelector(".contact");
+footer.style.backgroundColor = "#d7ddf3";
+
+console.log(footer);
+
+var tagline = document.querySelector(".tagline");
+tagline.innerText = "We got cha give us a call!";
+console.log(tagline);
+
+var address = document.querySelector("address");
+address.style.lineHeight = "1.5em";
+address.style.fontSize = "1.75em";
+console.log(address);
+
+var everyDay = document.querySelector("h3");
+everyDay.innerHTML = "We're here for you <em>every day</em> of the week";
+console.log(everyDay);
